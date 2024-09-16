7ad08edad0e2003889699f083d803b455c10f533
Lorianss <lorianss@mail.ru>
2024-09-16 12:28:15 +0300

    Problem

---
+++ Problem.py
@@ -0,0 +1,21 @@
+import random
+import heapq
+import math
+import sys
+from collections import defaultdict, deque, Counter
+from itertools import combinations
+
+class Problem:
+
+    def __init__(self, initial=None, goal=None, **kwds):
+        self.__dict__.update(initial=initial, goal=goal, **kwds)
+
+    def actions(self, state):        raise NotImplementedError
+    def result(self, state, action): raise NotImplementedError
+    def is_goal(self, state):        return state == self.goal
+    def action_cost(self, s, a, s1): return 1
+    def h(self, node):               return 0
+
+    def __str__(self):
+        return '{}({!r}, {!r})'.format(
+            type(self).__name__, self.initial, self.goal)
